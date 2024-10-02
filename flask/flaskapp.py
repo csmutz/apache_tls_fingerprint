dc9130036ed98e93c6bf9777735d6e924e10abca
Charles Smutz <csmutz@gmail.com>
2024-10-01 20:30:32 -0600

    add handshake RTT

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -1,18 +1,18 @@
 from flask import Flask, request
 import json
-import ja
+import fingerprints 
 
 app = Flask(__name__)
 
 @app.route('/') 
 def hello_world():
-    vars = ["SSL_SESSION_ID", "SSL_CLIENTHELLO_VERSION", "SSL_CLIENTHELLO_CIPHERS", "SSL_CLIENTHELLO_EXTENSIONS", "SSL_CLIENTHELLO_GROUPS", "SSL_CLIENTHELLO_EC_FORMATS", "SSL_CLIENTHELLO_SIG_ALGOS", "SSL_CLIENTHELLO_ALPN", "SSL_CLIENTHELLO_VERSIONS"]
+    vars = ["SSL_SESSION_ID", "SSL_CLIENTHELLO_VERSION", "SSL_CLIENTHELLO_CIPHERS", "SSL_CLIENTHELLO_EXTENSIONS", "SSL_CLIENTHELLO_GROUPS", "SSL_CLIENTHELLO_EC_FORMATS", "SSL_CLIENTHELLO_SIG_ALGOS", "SSL_CLIENTHELLO_ALPN", "SSL_CLIENTHELLO_VERSIONS", "SSL_HANDSHAKE_RTT"]
     data = {}
     for v in vars:
         data[v] = request.environ.get(v)
-    data['ja3'] = ja.ja3(data)
-    data['ja4'] = ja.ja4(data)
-    data['optimal'] = ja.optimal(data)
+    data['ja3'] = fingerprints.ja3(data)
+    data['ja4'] = fingerprints.ja4(data)
+    data['minimal'] = fingerprints.minimal(data)
     return json.dumps(data)
 
 if __name__ == '__main__':
